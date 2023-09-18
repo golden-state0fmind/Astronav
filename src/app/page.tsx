f75b74b4fd8ae74c9787a08329d1a77875d3f3e1
golden-state0fmind <areyes2009@yahoo.com>
2023-09-18 15:47:19 -0700

    renders current shapes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
+import { HexagonThreeD } from "@/components/HexagonThreeD";
+import { SpaceLake } from "@/components/SpaceLake";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      
-      </div>
+    <main className="w-full h-full">
+        <HexagonThreeD />
     </main>
   )
 }
