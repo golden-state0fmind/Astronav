51801c0faeaf6bf9d969138ff81662cc3a8bb317
golden-state0fmind <areyes2009@yahoo.com>
2023-09-21 03:14:40 -0700

    adds new component for shapes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
-import { HexagonThreeD } from "@/components/HexagonThreeD";
-import { SpaceLake } from "@/components/SpaceLake";
 import '../styles/styles.css';
 import { ImmersiveScroll } from "@/components/ImmersiveScroll";
 
 export default function Home() {
   return (
     <>
-      {/* <HexagonThreeD /> */}
       <ImmersiveScroll />
-
       <main>
         <header>
-          <h1>🚀</h1>
+          <h1>🚀 Space Facts 🚀</h1>
         </header>
 
-
         <blockquote>
-          <p>I like making stuff and putting it on the internet</p>
+          <p>🚀 🚀 🚀 🚀 🚀 🚀</p>
         </blockquote>
 
         <section>
-          <h2>Mission Statement</h2>
+          <h2>🚀 Space Facts will go here 🚀</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
@@ -36,13 +31,13 @@ export default function Home() {
         </section>
 
         <section className="light">
-          <h2>Services</h2>
+          <h2>🚀 Space Stuff 🚀</h2>
 
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
 
-          <h2>🏆 Accomplishments</h2>
+          <h2>🚀 More Space Facts 🚀</h2>
 
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -51,21 +46,21 @@ export default function Home() {
         </section>
 
         <blockquote>
-          <p>The best way out is always through <br/>-Robert Frost</p>
+          <p>Success is not final, failure is not fatal: It is the courage to continue that counts.<br />- Winston Churchillx</p>
         </blockquote>
 
         <section className="left">
-          <h2>Contact</h2>
+          <h2>🚀</h2>
 
-          <h3>Email</h3>
+          <h3>🚀</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <h3>Phoneg</h3>
+          <h3>🚀</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <h3>Social Medias</h3>
+          <h3>🚀</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
@@ -73,7 +68,7 @@ export default function Home() {
         </section>
 
         <blockquote>
-          <p>Add last message here.</p>
+          <p>🚀 🚀 🚀 🚀 🚀</p>
         </blockquote>
 
 
